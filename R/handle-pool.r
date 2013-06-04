1e5a9eb68c6052e8804eaae55ce7a0cd23ec09ef
Alex Brown <alexbbrown@apple.com>
2013-06-03 23:04:34 -0700

    added notes about not using multi with find_handle, and what might be done about it

--- R/handle-pool.r
+++ R/handle-pool.r
@@ -16,6 +16,12 @@ empty_pool()
 find_handle <- function(url) {
   handle_name <- build_url(parse_url(url)[c("scheme", "hostname", "port")])
 
+  # Note: if multi is in use, the handle pool may contain handles that are already 
+  # in use, and should not be re-used.  On the other hand, when handles have 
+  # concluded their operation and are popped from the multi, they can be re-used
+  # which can help take advantage of HTTP/1.1.  The pool might thus contain 
+  # multiple handles for a given scheme/hostname/port.  None of this is supported
+  # right now, so find_handle should not be used with multi.
   if (exists(handle_name, handle_pool)) {
     handle <- handle_pool[[handle_name]]
   } else {
