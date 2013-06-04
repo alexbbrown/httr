b51c91644854125a79c40a8b9e71a9ea921ced37
Alex Brown <alexbbrown@apple.com>
2013-06-03 23:44:32 -0700

    added make_request_deferred to allow deferred requests

---
+++ R/http--request-deferred.r
@@ -0,0 +1,92 @@
+#' @param action function with (at least) arguments \code{handle}, \code{url},
+#'   \code{opts}, which should return a list of \code{curl} and \code{response}
+#'   first the curl handle should be \code{perform}ed and then the reponse
+#'   function should be executed, to return binary content from the specified
+#'  request. \code{make_request} will take care of resetting the handle's
+#'   config after the request is made.
+#' @example
+#' deferred_request <- httr:::make_request_deferred(method="GET",handle=handle$handle,url=handle$url) -> X
+#' curlPerform(curl=deferred_request$curl)
+#' deferred_request$response()
+make_request_deferred <- function(method, handle, url, ..., config = list()) {
+  stopifnot(is.handle(handle))
+  stopifnot(is.character(url), length(url) == 1)
+  
+  # to avoid handle re-use, always duplicate the handle
+	handle$handle <- dupCurlHandle(handle$handle)
+
+  # Sign request, if needed
+  if (!is.null(config$signature)) {
+    signed <- config$signature(method, url)
+    url <- signed$url
+    config <- c(config, signed$config)
+
+    config$signature <- NULL
+  }
+
+  # Figure out curl options --------------------------------------------------
+  opts <- default_config()
+  opts$customrequest <- toupper(method)
+  opts$url <- url
+
+  # Action config override defaults
+  config_f <- match.fun(str_c(tolower(method), "_config"))
+  action_config <- config_f(...)
+  opts <- modifyList(opts, action_config)
+
+  # Config argument overrides everything
+  opts <- modifyList(opts, config)
+
+  # But we always override headerfunction and writefunction
+  hg <- basicHeaderGatherer()
+  opts$headerfunction <- hg$update
+  buffer <- binaryBuffer()
+  opts$writefunction <-
+    getNativeSymbolInfo("R_curl_write_binary_data")$address
+  opts$writedata <- buffer@ref
+
+  # Must always reset the handle config, even if something goes wrong
+  # ABB: not sure how to reconcile this with the deferred model
+  #on.exit(reset_handle_config(handle, opts))
+
+  # Perform request and capture output ---------------------------------------
+  curl_opts <- curlSetOpt(curl = NULL, .opts = opts)
+
+  is_post <- isTRUE(attr(action_config, "post"))
+  if (is_post) {
+		stop("POST not supported yet")
+    body <- attr(action_config, "body")
+    style <- attr(action_config, "style")
+    .postForm(handle$handle, curl_opts, body, style)
+    # reset configuration after post - again not sure how
+    # this fits, but it certainly must.
+    curlSetOpt(httppost = NULL, post = NULL, postfields = NULL,
+      curl = handle$handle)
+  } else {
+    curlSetOpt(curl = handle$handle, .opts = curl_opts$values)
+  }
+
+	return( 
+	  list(
+			curl=handle$handle,
+			response=function() {
+				content <- as(buffer, "raw")
+				info <- last_request(handle)
+				times <- request_times(handle)
+				headers <- insensitive(as.list(hg$value()))
+				status <- as.numeric(str_extract(headers$status, "[0-9]+"))
+
+				response(
+					url = info$effective.url,
+					handle = handle,
+					status_code = status,
+					headers = headers,
+					cookies = parse_cookies(info$cookielist),
+					content = content,
+								times = times,
+								config = config
+				)
+			}
+		)
+	)
+}
